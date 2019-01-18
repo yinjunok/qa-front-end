c7c1784ce7f3e811101dbbdf8f9c8119c4fc449e
Yin Jun <yinjunok@foxmail.com>
2019-01-18 17:15:26 +0800

    删除不必要的代码

--- layout/question-layout/SecondNav.tsx
+++ layout/question-layout/SecondNav.tsx
@@ -3,25 +3,6 @@ import { MdModeEdit } from 'react-icons/md';
 import { Button } from '../../components';
 import * as css from './styles.less';
 
-// class SecondNav extends React.Component {
-//   render() {
-//     return (
-//       <div className={css.secondNav}>
-//         <h1 className={css.questionTitle}>为什么中国要启动土豆主粮化战略？</h1>
-//         <div className={css.buttonGroup}>
-//           <Button>
-//             关注问题
-//           </Button>
-//           <Button type='blue'>
-//             <MdModeEdit className={css.buttonIcon} />
-//             写回答
-//           </Button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 const toTop = () => {
 	let cur = window.pageYOffset;
 	let target = 0;
